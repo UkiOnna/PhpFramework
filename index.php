54150475305a2e4634c2e3551e1558947bd53c85
UkiOnna <sikirovtemirlan@mail.ru>
2019-09-25 21:47:59 +0600

    Signed-off-by: UkiOnna <sikirovtemirlan@mail.ru>

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
 <?php
+
+
+use Medoo\Medoo;
+
+include "vendor/autoload.php";
+//
+//function callClass($name)
+//{
+//    $name = Inflector::classify($name);;
+//    if (!class_exists($name)) {
+//        echo "NO";
+//        exit();
+//    }
+//
+//    $class=new $name();
+//
+//    if(!method_exists($class,"getName")){
+//        echo "METHOD";
+//        exit();
+//    }
+//    return $class->getName();
+//}
+//
+//echo callClass("first");
+
+$cfg=include "database.php";
+$db=new Medoo($cfg);
+
+$db->delete("users",[
+    "username"=>"manager"
+]);
+//$db->insert("users",[
+//   "username"=>"manager",
+//   "name"=>"LEXA",
+//   "password"=>"123"
+//]);
+
+$db->update("users",[
+    "name"=>"BORYA"
+],[
+    "username"=>"manager"
+]);
+$users=$db->select("users","*");
+foreach ($users as$user){
+    foreach ($user as $key=>$value){
+        echo "$key => $value <br>";
+    }
+    echo "<hr />";
+}
+print_r($users);
